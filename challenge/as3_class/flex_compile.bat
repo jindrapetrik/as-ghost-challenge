63b42a3e191a65810093401eb598472f04e4acee
Jindra Petřík <jindra.petrik@gmail.com>
2018-01-26 10:04:55 +0100

    AS3 class version - Flex compiled file has same name as FLA SWF name

--- challenge/as3_class/flex_compile.bat
+++ challenge/as3_class/flex_compile.bat
@@ -3,7 +3,7 @@ for %%X in (mxmlc.exe) do (set FOUND=%%~$PATH:X)
 
 if "%found%"=="" goto notfound
 echo on
-mxmlc.exe -debug=false -omit-trace-statements=false -static-link-runtime-shared-libraries=true -optimize=false -output ghost_as3_class.swf com/jpexs/flash/mystery/Ghost.as -source-path .
+mxmlc.exe -debug=false -omit-trace-statements=false -static-link-runtime-shared-libraries=true -optimize=false -output ghost_as3_class_challenge.swf com/jpexs/flash/mystery/Ghost.as -source-path .
 @echo off
 goto two
 :notfound
